07774a3fc7c9d1e1cb19b286e80ab3a795d23f1f
gragas <tdf.tomfischer@gmail.com>
2015-09-01 14:14:09 -0500

    Updated main.py and menu.py to work with the new version of Buffalo.

--- main.py
+++ main.py
@@ -7,26 +7,19 @@ from menu import Menu
 def main():
 
     while not utils.end:
-        utils.logic()
-        utils.update()
-        utils.render()
+        utils.scene.logic()
+        utils.scene.update()
+        utils.scene.render()
         utils.delta = utils.clock.tick( utils.FRAMES_PER_SECOND )
 
 if __name__ == "__main__":
     
-    if not utils.init(
-        logic_func=Menu.logic,
-        update_func=Menu.update, 
-        render_func=Menu.render,
-        ):
+    if not utils.init():
         print('buffalo.utils failed to initialize')
         pygame.quit()
         exit()
 
-    if not Menu.init():
-        print('Menu failed to initialize')
-        pygame.quit()
-        exit()
+    utils.set_scene( Menu() )
 
     main()
 
--- menu.py
+++ menu.py
@@ -1,58 +1,26 @@
 import pygame
 
 from buffalo import utils
+from buffalo.scene import Scene
 from buffalo.label import Label
 from buffalo.button import Button
 
-class Menu:
+class Menu(Scene):
 
-    BACKGROUND_COLOR = (25, 150, 25)
-
-    labels = set()
-    buttons = set()
-    
-    @staticmethod
-    def init():
-        Menu.labels.add(
+    def __init__(self):
+        super().__init__()
+        self.labels.add(
             Label(
                 (5, 5),
                 "Adept 0.0 Alpha + 29 August 2015",
             )
         )
-        return True
 
-    @staticmethod
-    def logic():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                utils.end = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    utils.end = True
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                for button in Menu.buttons:
-                    if button.get_rect().collidepoint( mouse_pos ):
-                        button.set_selected(True)
-            elif event.type == pygame.MOUSEBUTTONUP:
-                mouse_pos = pygame.mouse.get_pos()
-                for button in Menu.buttons:
-                    button.set_selected(False)
-                    if button.get_rect().collidepoint( mouse_pos ):
-                        if button.func is not None:
-                            button.func()
+    def on_escape(self):
+        exit()
 
-    @staticmethod
-    def update():
+    def update(self):
         pass
 
-    @staticmethod
-    def render():
-        utils.screen.fill( Menu.BACKGROUND_COLOR )
-
-        for label in Menu.labels:
-            label.blit( utils.screen )
-        for button in Menu.buttons:
-            button.blit( utils.screen )
-
-        pygame.display.update()
+    def blit(self):
+        pass
