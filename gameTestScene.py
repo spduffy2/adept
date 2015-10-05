ca468b0f9e72287f367078d6a2816b34acb22f35
spduffy2 <spduffy2@illinois.edu>
2015-10-05 14:10:10 -0500

    added character selection and creation

--- gameTestScene.py
+++ gameTestScene.py
@@ -5,6 +5,7 @@ from buffalo import utils
 from buffalo.scene import Scene
 from buffalo.label import Label
 
+from saves import Saves
 from chunk import Chunk
 from camera import Camera
 from mapManager import MapManager
@@ -13,20 +14,24 @@ from pluginManager import PluginManager
 from playerCharacter import PlayerCharacter
 
 class GameTestScene(Scene):
-    def __init__(self):
+    def __init__(self, pc):
         Scene.__init__(self)
         self.BACKGROUND_COLOR = (0, 0, 0, 255)
         PluginManager.loadPlugins()
         Camera.init()
-        self.pc = PlayerCharacter(
-            name="Tom",
-            fPos=(float(utils.SCREEN_M[0]), float(utils.SCREEN_M[1])),
-            size=(32, 64),
-        )
+        pc = Saves.unstore(pc, "characters")
+        self.pc = pc if pc is not None else PlayerCharacter(
+                name="Sean",
+                fPos=(float(utils.SCREEN_M[0]), float(utils.SCREEN_M[1])),
+                size=(32, 64),
+                speed=.25,
+                color=(255,0,0,255),
+            )
         Camera.lock(self.pc)
         MapManager.loadChunks(0,0)
 
     def on_escape(self):
+        Saves.store(self.pc)
         sys.exit()
 
     def update(self):
