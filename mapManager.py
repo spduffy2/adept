50d7dbe17f9780f0a440a31653c17bd2bd23ba8c
gragas <tdf.tomfischer@gmail.com>
2015-10-06 19:40:39 -0500

    Fixed bug where chunk coordinates (x and y) were flipped for all chunks.

--- mapManager.py
+++ mapManager.py
@@ -5,16 +5,19 @@ class MapManager:
     Manages chunks and map loading
     """
 
-    BASE_PATH          = ["maps"] # BASE_PATH is a list version of the base path in
-                                  # which MapManager will search for chunks. For example,
-                                  # if the base path should be maps/otherfolder/ then
-                                  # BASE_PATH should equal ["maps", "otherfolder"]
-    maps                = []      # Maps is a list of all Map's found in within BASE_PATH
-    activeMap           = None    # activeMap is the active map // lol
-    LC_WIDTH, LC_HEIGHT = 3,3     # LC_WIDTH and LC_HEIGHT are the maximum width and height
-                                  # of loadedChunks, which contains Chunk's loaded into memory
+    BASE_PATH    = ["maps"] # BASE_PATH is a list version of the base path in
+                            # which MapManager will search for chunks. For example,
+                            # if the base path should be maps/otherfolder/ then
+                            # BASE_PATH should equal ["maps", "otherfolder"]
+    maps          = []      # Maps is a list of all Map's found in within BASE_PATH
+    activeMap     = None    # activeMap is the active map // lol
 
-    loadedChunks        = [[None]*3 for _ in range(3)]
+    loadedChunks  = [[None]*5 for _ in range(5)]
+
+    # LC_WIDTH and LC_HEIGHT are the maximum width and height
+    # of loadedChunks, which contains Chunk's loaded into memory
+    LC_WIDTH      = len(loadedChunks[0])
+    LC_HEIGHT     = len(loadedChunks)
 
     @staticmethod
     def loadMaps():
@@ -34,7 +37,7 @@ class MapManager:
         CHUNK_PATH_LIST = MapManager.BASE_PATH + [map_name, "chunks"]
         MapManager.maps.append(Map(map_name, CHUNK_PATH_LIST))
 
-    #Reloads loaded chunks in grid around central chunk (inputs use chunk coordinates)
+    # Reloads loaded chunks in grid around central chunk (inputs use chunk coordinates)
     @staticmethod
     def loadChunks(centralx, centraly):
         ybegin = centraly - int(MapManager.LC_HEIGHT/2)
@@ -43,7 +46,7 @@ class MapManager:
         xend   = centralx + int(MapManager.LC_WIDTH/2)
         for LCy, chunky in enumerate(range(ybegin, yend)):
             for LCx, chunkx in enumerate(range(xbegin, xend)):
-                MapManager.loadedChunks[LCx][LCy] = Chunk(chunkx, chunky)
+                MapManager.loadedChunks[LCy][LCx] = Chunk(chunkx, chunky)
 
 from Map import Map
 from pluginManager import PluginManager
