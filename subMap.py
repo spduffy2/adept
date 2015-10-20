b49c50709e580c325e26003340893415268dde17
Benjamin Congdon <bencongdon96@gmail.com>
2015-10-20 10:44:25 -0500

    Rendering of submaps

--- subMap.py
+++ subMap.py
@@ -1,19 +1,23 @@
 from serializable import Serializable
 from mapManager import MapManager
+from buffalo import utils
 
 class SubMap:
 
     PATH = ["subMaps"]
+    TILE_SIZE = 16
 
     def __init__(sizeX,sizeY,id,posX=0,posY=0):
         self.size = sizeX,sizeY
         self.pos = posX,posY
         self.tileMap = [[None for _x in range(self.sizeX)] for _y in range(self.sizeY)]
         self.fromFile()
+        self.surface = utils.empty_surface((sizeX * SubMap.TILE_SIZE, sizeY * SubMap.TILE_SIZE))
 
     def toFile(self):
         LOAD_PATH = MapManager.BASE_PATH + [MapManager.activeMap.name, SubMap.PATH]
         url = os.path.join(*list(LOAD_PATH + [id + '.smap']))
+
         output = ""
         for y in range(len(self.size[1])):
             for x in range(len(self.size[0])):
@@ -22,12 +26,24 @@ class SubMap:
 
         with open(url,'w+') as subMapFile:
             subMapFile.write(output)
-            
+
     def fromFile(self):
+        LOAD_PATH = MapManager.BASE_PATH + [MapManager.activeMap.name, SubMap.PATH]
+        url = os.path.join(*list(LOAD_PATH + [id + '.smap']))
+
         tiles = list()
-        input = "" #READ FILE
-        for tileString in input.split():
-            tiles.append(Serializable.to_object(tileString))
+        with open(url,'r') as subMapFile:
+            for tileLine in subMapFile:
+                tiles.append(Serializable.to_object(tileLine))
 
         for tile in tiles:
             self.tileMap[tile.pos[0]][tile.pos[1]] = tile
+
+    def render(self):
+        for y in range(len(self.size[1])):
+            for x in range(len(self.size[0])):
+                self.surface.blit(self.tileMap[x][y].surface, (SubMap.TILE_SIZE * x, SubMap.TILE_SIZE * y))
+
+    def blit(self, dest, pos):
+        dest.blit( self.surface, pos )
+
--- tile.py
+++ tile.py
@@ -1,8 +1,12 @@
 from serializable import Serializable
+from buffalo import utils
+from subMap import SubMap
+
 class Tile(Serializable):
 	def __init__(pos,type_id):
 		self.pos = pos
 		self.type = type_id
+		self.surface = utils.empty_surface((SubMap.TILE_SIZE,SubMap.TILE_SIZE))
 
 	def loadIDProperties(self):
 		pass
