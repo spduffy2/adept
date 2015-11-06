f125c42f7798e6a53b670edbc4b3ebc3c27f46c1
Benjamin Congdon <bencongdon96@gmail.com>
2015-11-06 11:44:57 -0600

    A few example tests

---
+++ test_item.py
@@ -0,0 +1,20 @@
+from item import Item
+import pygame
+import unittest
+from buffalo import utils
+import random
+
+utils.init(
+            caption='Adept',
+            fullscreen=True
+        )
+
+class TestItem:
+    def test_init(self):   
+        i = Item("axe")
+        assert i.name == "axe"
+        assert i.quantity == 1
+
+    def test_unknown_item(self):
+    	i = Item(str(random.random))
+    	assert i.surface is not None
