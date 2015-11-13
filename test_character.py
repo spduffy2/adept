1d810e1f6f7b085eb637f3a741bf9cdfef0cb685
Thomas David Fischer <tdf.tomfischer@gmail.com>
2015-11-12 20:22:10 -0600

    Fixed bad merge.

--- test_character.py
+++ test_character.py
@@ -1,3 +1,4 @@
+from inventory import Inventory
 from item import Item
 import pygame
 import unittest
@@ -23,7 +24,7 @@ class TestCharacter:
     def test_saveing(self):
         self.init()
         self.c.characterName.label.text = "Test"
-        self.c.create_character(Inventory())
+        self.c.create_character()
         assert os.path.isfile(os.path.join("characters","Test")) is True
         if os.path.isfile(os.path.join("characters","Test")) is True:
             os.remove(os.path.join("characters","Test"))
@@ -32,18 +33,18 @@ class TestCharacter:
 
     def test_surface(self):
         from playerCharacter import PlayerCharacter
-        char = PlayerCharacter(Inventory())
+        char = PlayerCharacter()
         assert char.surface is not None
 
     def test_inventory__pc_reference(self):
         from playerCharacter import PlayerCharacter
-        char = PlayerCharacter(Inventory())
+        char = PlayerCharacter()
         assert char.inventory.playerCharacter() == char
 
     def test_serialize(self):
         from playerCharacter import PlayerCharacter
         from serializable import Serializable
-        char = PlayerCharacter(Inventory())
+        char = PlayerCharacter()
         j = char.serialize()
         decoded = Serializable.deserialize(j)
         assert char.name == decoded.name
