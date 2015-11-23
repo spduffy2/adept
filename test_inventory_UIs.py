78b13be907b3feeca307ea8c4f5183188141a9ae
Benjamin Congdon <bencongdon96@gmail.com>
2015-11-23 10:04:30 -0800

    More progress in InventoryUI tests

--- test_inventory_UIs.py
+++ test_inventory_UIs.py
@@ -3,9 +3,13 @@ from hotbarUI import HotbarUI
 from buffalo import utils
 from inventory import Inventory
 from guiManager import GUIManager
+from item import Item
 
 utils.init()
 
+#####################
+###INVENTORY TESTS###
+#####################
 def test_init_inventory():
 	i = Inventory()
 	g = GUIManager()
@@ -17,6 +21,32 @@ def test_init_inventory():
 	assert ui.surface.get_size() == (ui.inventory.INV_SIZE_X * (InventoryUI.BUTTON_SIZE + InventoryUI.PADDING) + InventoryUI.PADDING,
 		ui.inventory.INV_SIZE_Y * (InventoryUI.BUTTON_SIZE + InventoryUI.PADDING) + InventoryUI.PADDING)
 
+def test_getGUIPosFromItemPos():
+	i = Inventory()
+	g = GUIManager()
+	ui = InventoryUI(i,g)
+	assert ui.getGUIPosFromItemPos((0,0)) == (6,6)
+	assert ui.getGUIPosFromItemPos((3,3)) == (120,120)
+	assert ui.getGUIPosFromItemPos((10,1)) == (386, 44)
+
+def test_getItemFromGUIPos():
+	i = Inventory()
+	g = GUIManager()
+	ui = InventoryUI(i,g)
+	item1 = Item("1")
+	item2 = Item("2")
+	item3 = Item("3")
+	i.items[0][0] = item1
+	i.items[2][2] = item2
+	i.items[5][1] = item3
+	assert ui.getItemFromGUIPos(ui.getGUIPosFromItemPos((0,0))) == item1
+	assert ui.getItemFromGUIPos(ui.getGUIPosFromItemPos((2,2))) == item2
+	assert ui.getItemFromGUIPos(ui.getGUIPosFromItemPos((5,1))) == item3
+	assert ui.getItemFromGUIPos(ui.getGUIPosFromItemPos((1,1))) is None
+
+##################
+###HOTBAR TESTS###
+##################
 def test_init_hotbar():
 	i = Inventory()
 	g = GUIManager()
@@ -26,3 +56,5 @@ def test_init_hotbar():
 	assert ui.itemRects is not None
 	assert ui.guiManager
 	assert ui.selectedIndex == 0
+	assert ui.surface.get_size() == (ui.inventory.INV_SIZE_X * (InventoryUI.BUTTON_SIZE + InventoryUI.PADDING) + InventoryUI.PADDING,
+		1 * (InventoryUI.BUTTON_SIZE + InventoryUI.PADDING) + InventoryUI.PADDING)
