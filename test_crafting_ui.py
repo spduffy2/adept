c982999c9dacfd18d568d23c9eebf2435febb61d
Benjamin Congdon <bencongdon96@gmail.com>
2015-11-24 20:38:03 -0800

    Crafting UI / Recipe system tests

--- test_crafting_ui.py
+++ test_crafting_ui.py
@@ -3,6 +3,7 @@ from craftingUI import CraftingUI
 from inventory import Inventory
 from recipe import Recipe
 from recipeManager import RecipeManager
+from item import Item
 
 utils.init()
 
@@ -14,7 +15,15 @@ def test_init():
 	assert len(c.tileRecipes) == len(RecipeManager.RECIPES)
 
 def test_fail_recipe():
-	pass
+	i = Inventory()
+	c = CraftingUI(i)
+	r = RecipeManager.getRecipe("pickaxe")
+	assert r.canCraft(i) == False
 
 def test_success_recipe():
-	pass
+	i = Inventory()
+	c = CraftingUI(i)
+	i.addItem(Item("stick",quantity=1))
+	i.addItem(Item("stone",quantity=5))
+	r = RecipeManager.getRecipe("pickaxe")
+	assert r.canCraft(i) == True
